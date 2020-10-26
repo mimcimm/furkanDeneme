e72c4f0ae390624b49376fd1cf28b3122a31dc7a
Abdurrahman Kurtvuran <a.kurtvuran@mepsan.com.tr>
2020-10-26 09:07:25 +0300

    version 1.1

--- furkanTest/main.cpp
+++ furkanTest/main.cpp
@@ -1,8 +1,10 @@
 #include <QCoreApplication>
-
+#include <QDebug>
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+//Furkan
+    //Test2
+    qDebug()<< "code test";
     return a.exec();
 }
